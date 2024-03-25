62a806568c34f1529f8b3d5956cbd02ba390a357
Zuhaib-Uddin <161199131+Zuhaib-Uddin@users.noreply.github.com>
2024-03-25 07:36:39 +0500

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-//simple calculator
-var firstNumber = 5;
-var secondNumber = 6;
-console.log('the answer of sum is :', firstNumber + secondNumber);
-console.log('the answer of substract is :', firstNumber - secondNumber);
-console.log('the answer of multiplacion is :', firstNumber * secondNumber);
-console.log('the answer of division is :', firstNumber / secondNumber);
